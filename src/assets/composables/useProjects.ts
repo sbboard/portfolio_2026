d8dcb84706c9030ff688fcee1273ebef24e14351
Colin Buffum <colin@direqt.ai>
2025-12-26 15:17:12 -0500

    feat: update index.html title and add project interface with sample data

---
+++ src/assets/composables/useProjects.ts
@@ -0,0 +1,39 @@
+export interface Project {
+    name: string;
+    date: string;
+    description: string;
+    tech: string[];
+    image: string;
+    video: string;
+    link?: string;
+    repo?: string;
+}
+
+export const projects: Project[] = [
+    {
+        name: 'Salvaged Parts',
+        date: '5/01/2022',
+        description: 'Artsy web toy about an astronaut making music.',
+        tech: ['Javascript'],
+        image: 'https://gang-fight.com/assets/contentImages/salvage.jpg',
+        video: 'salvage.mp4',
+        link: 'https://gang-fight.com/projects/salvaged_parts/',
+    },
+    {
+        name: 'PC-98 Emulation For Beginners',
+        date: '4/01/2020',
+        description: 'Tutorial website for getting started with PC-98 emulation.',
+        tech: ['HTML5', 'CSS3'],
+        image: 'https://gang-fight.com/assets/contentImages/pc982024.png',
+        video: 'salvage.mp4',
+        link: 'https://gang-fight.com/projects/98faq/',
+    },
+    {
+        name: 'Parker Kelly Website',
+        date: '2/01/2018',
+        description: 'Portfolio website for an advertising creative.',
+        tech: ['HTML5', 'CSS3', 'Javascript', 'PHP', 'JQuery'],
+        image: 'https://colinbuffum.com/img/parker.jpg',
+        video: 'salvage.mp4',
+    },
+];
