2c61d9c2368f1e8fb72d8771867e48454e79fee5
Colin Buffum <colin@direqt.ai>
2026-01-06 12:58:15 -0500

    fix: add click listener for dropdown to close when clicking outside

--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -3,9 +3,21 @@ import { ref } from 'vue';
 import { CURRENT_THEME_INDEX, themes } from '@/utils/styleConfig';
 
 const isOpen = ref(false);
+const clickListener = (e: MouseEvent) => {
+    const dropdown = document.querySelector('.dropdown');
+    if (dropdown && !dropdown.contains(e.target as Node)) {
+        isOpen.value = false;
+        document.removeEventListener('click', clickListener);
+    }
+};
 
 const toggleDropdown = () => {
     isOpen.value = !isOpen.value;
+    if (isOpen.value) {
+        document.addEventListener('click', clickListener);
+    } else {
+        document.removeEventListener('click', clickListener);
+    }
 };
 
 const selectTheme = (index: number) => {
